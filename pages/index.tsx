09384312ee521c365ef6e1da2779e3da424116bf
Derek Stotz <okiedokieiguess@gmail.com>
2023-02-21 20:23:50 -0600

    Bootstrap

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+import "./index.css";
+
+export default function Home() {
+  return (
+    <main>
+      <h1>Heartland Retail UI Extension Demo</h1>
+    </main>
+  );
+}
